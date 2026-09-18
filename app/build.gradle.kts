096d4577a012056f984f0bfebcdb01f979a4a54a
Shivaanshe <shivaanshtiwari2006@gmail.com>
2026-09-19 00:26:06 +0530

    ui improvement and dynamic cache according user v3.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.song"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.4.2"
+        versionCode = 4
+        versionName = "3.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
