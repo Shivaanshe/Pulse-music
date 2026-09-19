cf1ae868633526c1e639a5beaf345ff1ea70deb2
Shivaanshe <shivaanshtiwari2006@gmail.com>
2026-09-19 22:16:38 +0530

    queue is now working v3.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.song"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "3.4.3"
+        versionCode = 5
+        versionName = "3.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
